136f942b7ef35f984dd7761d3c868c5209bc1a8d
Mark Dierolf <mdierolf@cloudquote.io>
2025-12-21 19:13:13 -0800

    Update `install.sh` to include required dependencies and clean up after installation

--- docker/install.sh
+++ docker/install.sh
@@ -2,7 +2,13 @@
 
 set -e
 
+apt update
+apt install -y git libportaudio2
+
 chmod 755 /dist/tr-writer
+
+apt remove -y build-essential
+apt autoremove -y
 
 rm -rf /var/cache/apt
 rm -rf /var/lib/apt/lists/*
